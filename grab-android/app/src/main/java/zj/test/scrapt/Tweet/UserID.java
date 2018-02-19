7ea758f161a089c661859e146be3f592cd7f02f5
linurm <343600434@qq.com>
2018-02-19 20:10:05 +0800

    更新了一下界面

--- grab-android/app/src/main/java/zj/test/scrapt/Tweet/UserID.java
+++ grab-android/app/src/main/java/zj/test/scrapt/Tweet/UserID.java
@@ -5,10 +5,8 @@ package zj.test.scrapt.Tweet;
  */
 
 public class UserID {
-    public static final String second_uid = "1789247505";
-    public static final String third_uid = "1772392290";
     public static final String first_uid = "3373931552";
+    public static final String second_uid = "1772392290";
+    public static final String third_uid = "1789247505";
     public static final String fourth_uid = "1726640331";
-
-
 }
